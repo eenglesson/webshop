b5813044f80b2b4bd2e80656de21e1b2d4d21469
eenglesson <eliasenglesson00@gmail.com>
2024-11-17 17:41:54 +0100

    cart

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,22 @@
+'use client';
+
 import './globals.css';
+import React, { Suspense } from 'react';
 import { Providers } from './providers';
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang='en'>
       <body>
         <div className='max-w-[1400px] mx-auto px-4 sm:px-8'>
-          {' '}
-          <Providers>{children}</Providers>
+          <Providers>
+            {/* Wrap children with Suspense */}
+            <Suspense fallback={<p>Loading...</p>}>{children}</Suspense>
+          </Providers>
         </div>
       </body>
     </html>

cda1d8f8df97c9d6458b8a58723aed30130f1401
eenglesson <eliasenglesson00@gmail.com>
2024-11-18 22:12:29 +0100

    cart page done

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState, Suspense } from 'react';
+import React, { Suspense } from 'react';
 import Navbar from '../components/Navbar';
 import { useCart } from '../context/CartContext';
 import Image from 'next/image';

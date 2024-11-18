d644b8e9604f14d2b2f7c7e89944c69a80ca0145
eenglesson <eliasenglesson00@gmail.com>
2024-11-18 22:33:26 +0100

    cart page done

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -5,7 +5,9 @@ import { CartPageClient } from './CartPageClient';
 export default function CartPage() {
   return (
     <>
-      <Navbar />
+      <Suspense fallback={<p>Loading navbar...</p>}>
+        <Navbar />
+      </Suspense>
       <h1 className='text-h4 sm:text-h3 mt-8 font-semibold mb-4'>
         Shopping Cart
       </h1>

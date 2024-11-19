665ae797451b65dc45f6265adabb420e2e73d911
eenglesson <eliasenglesson00@gmail.com>
2024-11-19 02:48:08 +0100

    wihoo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // app/layout.tsx
 'use client';
 import './globals.css';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Providers } from './providers';
 import { Toaster } from 'sonner';
 import Navbar from './components/Navbar';
@@ -15,7 +15,9 @@ export default function RootLayout({
     <html lang='en'>
       <body>
         <Providers>
-          <Navbar />
+          <Suspense fallback={<p>Loading...</p>}>
+            <Navbar />
+          </Suspense>
           <div className='max-w-[1400px] mt-4 sm:mt-8 mx-auto px-4 sm:px-8'>
             {children}
             <Toaster position='bottom-right' richColors />

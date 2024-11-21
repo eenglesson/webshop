0321d7683416811900a70541675b85e10ceec5aa
eenglesson <eliasenglesson00@gmail.com>
2024-11-21 02:08:54 +0100

    image fix

--- app/components/BigImage.tsx
+++ app/components/BigImage.tsx
@@ -6,13 +6,13 @@ export default function BigImage() {
     <>
       <div className='w-full h-[300px] overflow-hidden relative rounded-xl'>
         <Image
-          src='/ski.jpg'
+          src='/hikingImg.jpeg'
           alt='skiImage'
           fill
           style={{ objectFit: 'cover' }}
           priority
         />
-        <div className='absolute inset-0 flex items-center justify-start bg-black bg-opacity-25 text-white text-lg font-semibold p-4 sm:p-8'>
+        <div className='absolute inset-0 flex items-center justify-start bg-black bg-opacity-20 text-white text-lg font-semibold p-4 sm:p-8'>
           <div className=''>
             <h2 className='text-h5 sm:text-h3 font-'>Outdoor Clothing</h2>
             <p className='text-tiny sm:text-small font-medium'>
